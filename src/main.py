e5b58975373d8efa6f8e7aa58438b4099efea3fa
Erin Stuelke <erin_stuelke@med.unc.edu>
2019-05-28 15:54:40 -0400

    Consolidated template and outfile names into one method

--- src/main.py
+++ src/main.py
@@ -6,28 +6,26 @@ Module for creating Excel formula verification tests
 - Exports the tests to a word document
 """
 # TODO: Create UI for this
-
-from datetime import datetime
 from docx import Document
 from template_file import process_template_file
-from verification_document import create_document, create_filename
+from verification_document import create_document
+from utils import create_filename
 
 
 def main():
+    # NOTE: All the following information will come from the UI once it's created
     template_fname = (r'C:/Users/erins/OneDrive - University of North Carolina at Chapel Hill/Protocols and SOPs/' 
                 r'MARG-PTC-001 Primary Cell Spinoculation and Latency/MARG-PTC-001a-v1_1-Lewin Template.xlsx')
 
     out_dir = r'C:/Users/erins/OneDrive - University of North Carolina at Chapel Hill/Protocols and SOPs/' \
               r'MARG-PTC-001 Primary Cell Spinoculation and Latency/'
 
-    out_pre = datetime.now().strftime('%Y%m%d') + '_ELS_'
-    out_suf = '_' + datetime.now().strftime('%H%M%S')
-    outfile = out_dir + out_pre + r'list_formulas_and_names' + out_suf + '.xlsx'
-
-    template_data: dict = process_template_file(template_fname, outfile)
-
     template_name: str = 'test'
     template_desc: str = 'test'
+
+    ########
+    outfile: str = create_filename(out_dir, 'formulas and names', extension='xlsx')
+    template_data: dict = process_template_file(template_fname, outfile)
 
     filename: str = create_filename(out_dir, template_desc)
     document: Document = create_document(template_data, template_name, template_desc)
--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,20 @@
-from docx.text.run import Run
-from docx.table import _Cell as Cell
-from docx.styles.style import _ParagraphStyle as ParagraphStyle
-from docx.oxml.ns import nsdecls, qn
+from datetime import datetime
 from docx.oxml import parse_xml, OxmlElement
+from docx.oxml.ns import nsdecls, qn
+from docx.styles.style import _ParagraphStyle as ParagraphStyle
+from docx.table import _Cell as Cell
+from docx.text.run import Run
 
 
 def add_outline_level(self: ParagraphStyle, level: int):
     """
     Adds a method to the _ParagraphFormat class that adds the outline level attribute to the document's xml.
     python-docx does not have this natively available, therefore a custom function is needed.
+    Method is added dynamically to the ParagraphStyle class.
     :param self: _ParagraphFormat Class object
     :param level: Level at which the attribute is added, determines where item will reside in the document's TOC
     :return: N/A
     """
-    assert isinstance(self, ParagraphStyle)
     outline_level = parse_xml(f'<w:outlineLvl {nsdecls("w")} w:val="{level}" />')
     self._element.get_or_add_pPr().append(outline_level)
 
@@ -25,12 +26,11 @@ ParagraphStyle.add_outline_level = add_outline_level
 def add_field(self: Run, field: str):
     """
     Adds a Word Field to the Paragraph Run by modifying the document's xml
-    This feature is not part of docx yet and thus a custom function is needed
-
+    This feature is not part of docx yet and thus a custom function is needed.
+    Method is added dynamically to the Run class.
     :param self: Location (run) in document to add Field
     :param field: Specific field-type to add
     """
-    assert isinstance(self, Run)
     fld_char_begin = OxmlElement('w:fldChar')
     fld_char_begin.set(qn('w:fldCharType'), 'begin')
     instr_text = OxmlElement('w:instrText')
@@ -59,17 +59,31 @@ Run.add_field = add_field
 def shade_cell(self: Cell, color: str):
     """
     Shades the given cell with color.  openpyxl does not have this option natively.
-    This method modifies the xml of the document.
+    This method modifies the xml of the document.  Method is added dynamically to
+    the Cell class.
     :param self: cell to add color to
     :param color: str representing color in hex format
     :return: n/a
     """
-    assert isinstance(self, Cell)
     cell_color = parse_xml(f'<w:shd {nsdecls("w")} w:fill="{color}" />')
     self._tc.get_or_add_tcPr().append(cell_color)
 
 
 Cell.shade_cell = shade_cell
 
+
+def create_filename(directory: str, description: str, initials: str = 'ELS', extension: str = 'docx') -> str:
+    """
+    Sets the name of the file based on the current timestamp and directory
+    :param directory: Directory where document will be stored
+    :param description: Description of the document
+    :param initials: Initials of user
+    :param extension: File type extension (should be docx or xlsx)
+    :return: name of the document as string
+    """
+    if extension not in ['docx', 'xlsx']:
+        raise ValueError("Extension must be docx or xlsx")
+    now = datetime.now().strftime
+    return f"{directory}{now('%Y%m%d')}_{initials}_{description}_{now('%H%M%S')}.{extension}"
 
 
